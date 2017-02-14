06d50547b2cef4dd12e4c057e26fb51179ec77e9
Joel <joelfernandezreviejo@gmail.com>
2017-02-14 13:49:31 +0100

    Initial commit

---
+++ app/src/main/java/es/cice/moviedbfinalproject/model/Images.java
@@ -0,0 +1,83 @@
+
+package com.example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Images {
+
+    private String baseUrl;
+    private String secureBaseUrl;
+    private List<String> backdropSizes = null;
+    private List<String> logoSizes = null;
+    private List<String> posterSizes = null;
+    private List<String> profileSizes = null;
+    private List<String> stillSizes = null;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String getSecureBaseUrl() {
+        return secureBaseUrl;
+    }
+
+    public void setSecureBaseUrl(String secureBaseUrl) {
+        this.secureBaseUrl = secureBaseUrl;
+    }
+
+    public List<String> getBackdropSizes() {
+        return backdropSizes;
+    }
+
+    public void setBackdropSizes(List<String> backdropSizes) {
+        this.backdropSizes = backdropSizes;
+    }
+
+    public List<String> getLogoSizes() {
+        return logoSizes;
+    }
+
+    public void setLogoSizes(List<String> logoSizes) {
+        this.logoSizes = logoSizes;
+    }
+
+    public List<String> getPosterSizes() {
+        return posterSizes;
+    }
+
+    public void setPosterSizes(List<String> posterSizes) {
+        this.posterSizes = posterSizes;
+    }
+
+    public List<String> getProfileSizes() {
+        return profileSizes;
+    }
+
+    public void setProfileSizes(List<String> profileSizes) {
+        this.profileSizes = profileSizes;
+    }
+
+    public List<String> getStillSizes() {
+        return stillSizes;
+    }
+
+    public void setStillSizes(List<String> stillSizes) {
+        this.stillSizes = stillSizes;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
